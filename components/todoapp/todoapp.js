ea436ca87470dc3a60651cffbcf98fd6cd619d92
keleko34 <gregory.guidero@gmail.com>
2017-04-04 11:29:10 +0200

    update sub libraries and add default page route

--- components/todoapp/todoapp.js
+++ components/todoapp/todoapp.js
@@ -6,13 +6,27 @@
 
 function todoapp()
 {
+  var self = this;
   /* the main title */
   this.title = "todo";
+  this.fontSize = 40;
+  
+  this.multiple = true;
   
   /* a simple filter that returns uppercase string */
   this.filters.toUpperCase = function(v)
   {
     return v.toUpperCase();
+  }
+  
+  this.filters.addExC = function(v)
+  {
+    return v+"!!!!!";
+  }
+  
+  this.makebigger = function()
+  {
+    self.fontSize += 5;
   }
 }
 
